de508a800da50dbc7bc916f5279c1a370d13b991
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-10-07 13:57:49 +0100

    made use of ngFor directive

--- src/app/pages/jenzco-hotels/jenzco-hotel.component.ts
+++ src/app/pages/jenzco-hotels/jenzco-hotel.component.ts
@@ -8,7 +8,48 @@ import { IClient } from './clients';
 })
 export class JenzcoHotelComponent implements OnInit {
   hotelTitle: string = 'Jenzco Hotels and Suites';
-  clients: IClient[] = [];
+  clients: IClient[] = [
+    {
+      clientId: 1,
+      clientName: 'Luciano',
+      clientRoomCode: '11191',
+      ArrivalDate: 'Aug 19, 2021',
+      price: 56092.88,
+      starRating: 3.2,
+    },
+    {
+      clientId: 2,
+      clientName: 'Jummy',
+      clientRoomCode: '89023',
+      ArrivalDate: 'July 18, 2021',
+      price: 56309.88,
+      starRating: 4.2,
+    },
+    {
+      clientId: 3,
+      clientName: 'Chisom',
+      clientRoomCode: '11048',
+      ArrivalDate: 'April 02, 2021',
+      price: 15609.88,
+      starRating: 4.8,
+    },
+    {
+      clientId: 4,
+      clientName: 'Benedict',
+      clientRoomCode: '40022',
+      ArrivalDate: 'April 10, 2022',
+      price: 11000.55,
+      starRating: 3.7,
+    },
+    {
+      clientId: 5,
+      clientName: 'Khaid',
+      clientRoomCode: '65042',
+      ArrivalDate: 'October 15, 2020',
+      price: 30985.95,
+      starRating: 4.6,
+    },
+  ];
 
   constructor() {}
 
